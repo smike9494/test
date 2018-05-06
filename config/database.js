b05a2ac9b7e7f56426854a3899a688f39280caa2
Michael Sanders <smike9494@gmail.com>
2018-05-06 01:22:28 -0700

    mlabs

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost/grid',
+    database: 'mongodb://mike:123@ds215910.mlab.com:15910/article',
     secret: 'As Iron Sharpens Iron, So One Person Sharpens Another'
 }
