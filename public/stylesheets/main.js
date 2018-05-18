b5647beeccebf87885774bd5a160d1c429243733
Michael Sanders <smike9494@gmail.com>
2018-05-18 13:11:26 -0700

    updated title

--- public/stylesheets/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-            $('#btn1').on('click', function (e) {
-              $('#btn1').each(function () {
-                $(this).addClass('active');
-              });
-
-            });
-          });
--- z/z1Configurations/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    JWT_SECRET: 'As Iron Sharpens Iron, So One Person Sharpen Another'
-}
